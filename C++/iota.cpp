6785e377aad2b9671a1e85fbbe4183e1b7112de3
上野清雅 <seiga.ueno@gmail.com>
2019-04-11 20:09:42 +0900

    new file

--- C++/iota.cpp
+++ C++/iota.cpp
@@ -5,5 +5,9 @@
 using namespace std;
 
 int main(){
-    vector <int> vec;
+    vector <int> vec(10);
+    iota(vec.begin(),vec.end(),1);
+    for (int x: vec){
+        cout << x << endl;
+    }
 }
