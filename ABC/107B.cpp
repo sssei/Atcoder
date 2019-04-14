e194a20c1e0ed4233555a1e401505610deebc054
上野清雅 <seiga.ueno@gmail.com>
2019-04-14 23:40:59 +0900

    new file

--- ABC/107B.cpp
+++ ABC/107B.cpp
@@ -12,19 +12,35 @@ int main()
     for (int i = 0; i < H; i++){
         cin >> a.at(i);
     }
+
     int cnt = 0;
     for (int i = 0; i < H - cnt; i++){
         if(a.at(i - cnt) == string('.',W)){
             a.erase(a.begin() + i - cnt);
             cnt++;
+            cout << " hh" << endl;
         }
     }
     
     int cnt2 = 0;
+    
     for (int j = 0; j < W - cnt2; j++){
+        bool flag = true;
         for (int i = 0; i < H - cnt; i++){
-            if (a[i][j] != '.') break;
+            if (a[i][j - cnt2] != '.'){
+                flag = false;
+                break;
+            }
         }
-        continue 
+        if (!flag){
+            for (int i = 0; i < H - cnt; i++){
+                a[i].erase(a[i].begin() + j - cnt2);
+                cnt2++;
+            }
+        }
+    }
+
+    for (int i = 0; i < H - cnt; i++){
+        cout << a.at(i) << endl;
     }
 }
