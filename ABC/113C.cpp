4f60b14980c170a6999e8fd51773edd27b4d2608
上野清雅 <seiga.ueno@gmail.com>
2019-07-11 20:37:47 +0900

    new file

--- ABC/113C.cpp
+++ ABC/113C.cpp
@@ -1,15 +1,54 @@
+#include <cstdio>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 struct city{
   int id;
   int year;
-  bool operator<(const )
+  int number;
+  int prefecture;
+  bool operator<(city c){
+    return this->year < c.year;
+  }
 };
 
 struct Prefecture{
   int idx;
-  
+  vector<city> City;
+  void insert(city *c){
+    City.push_back(*c);
+    c->prefecture = idx;
+  }
 };
+
+int main(){
+  int N, M; cin >> N >> M;
+  vector<Prefecture> P(N);
+  vector<city> C(M);
+  for(int i = 0; i < N; i++){
+    P[i].idx = i;
+  }
+  for(int i = 0; i < M; i++){
+    C[i].id = i;
+    int pre; cin >> pre;
+    P[pre].insert(&C[i]);
+    cin >> C[i].year;
+  }
+  for(int i = 0; i < N; i++){
+    sort();
+    int len = P[i].City.size();
+    for(int j = 0; j < len; j++){
+      P[i].City[j].number = j;
+    } 
+  }
+  for(int i = 0; i < M; i++){
+    int ans = C[i].prefecture * 1000000;
+    ans += C[i].number;
+    printf("%12d\n", ans);
+  }
+}
+
 
