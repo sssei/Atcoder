a162172808acde2afeec93312afd07eb778e9efb
上野清雅 <seiga.ueno@gmail.com>
2019-08-02 16:31:03 +0900

    new file

---
+++ ABC/085C.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  int N,Y;
+  cin >> N >> Y;
+  int res1 = -1, res2 = -1, res3 = -1;
+  
+  for(int a = 0; a <= N; a++){
+    for(int b = 0; a + b <= N; b++){
+      int c = N - a - b;
+      int total = 10000 * a + 5000 * b + 1000 * c;
+      if(total == Y){
+	res1 = a;
+	res2 = b;
+	res3 = c;
+	break;
+      }
+    }
+  }
+
+  cout << res1 << " " << res2 << " " << res3 << endl;
+  
+}
