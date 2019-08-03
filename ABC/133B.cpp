9b5dc9beb61e7ed87b835972fbc854ec30f0b410
上野清雅 <seiga.ueno@gmail.com>
2019-08-03 23:17:19 +0900

    new file

--- ABC/133B.cpp
+++ ABC/133B.cpp
@@ -15,4 +15,14 @@ int main(){
       cin >> X[i][j];
     }
   }
+
+  for(int i = 0; i < N - 1; i++){
+    for(int j = i + 1; j < N; j++){
+      double dis = 0;
+      for(int k = 0; k < D; k++){
+	dis += pow(X[i][k] - X[j][k], 2);
+      }
+      dis = sqrt(dis);
+    }
+  }
 }
