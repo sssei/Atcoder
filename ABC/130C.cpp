94b61382e8401d445be5943a1ecd7a79dbe7c935
上野清雅 <seiga.ueno@gmail.com>
2019-08-18 13:56:21 +0900

    new file

--- ABC/130C.cpp
+++ ABC/130C.cpp
@@ -10,13 +10,7 @@ using namespace std;
 int main(){
   double  W,H,x,y;
   cin >> W >> H >> x >> y;
-  vector<double> area(4);
-  area[0] = y / H;
-  area[1] = 1 - y / H;
-  area[2] = x / W;
-  area[3] = 1 - x / W;
-  sort(area.begin(), area.end());
-  int  flag = 0;
-  if(area[0] == area[1]) flag = 1;
-  cout << area[2] * H * W << " " << flag << endl;
+  int flag = 0;
+  if(x == W/2 && y == H/2) flag = 1;
+  cout << W * H / 2 << " " << flag << endl;
 }
