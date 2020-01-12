4bb1693e6e79486c6ded79efb875628ee9e73513
上野清雅 <seiga.ueno@gmail.com>
2020-01-12 11:34:41 +0900

    add 058A.cpp

---
+++ ABC/058A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int MOD = 1000000007;
+
+int main(){
+  int a,b,c;
+  cin >> a >> b >> c;
+  if(b-a == c-b){
+    cout << "YES" << endl;
+  }else{
+    cout << "NO" << endl;
+  }
+}
