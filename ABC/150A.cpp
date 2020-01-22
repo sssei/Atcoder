dd114262b7780789804ccb7a1e5edfcf05e764ca
上野清雅 <seiga.ueno@gmail.com>
2020-01-22 12:44:48 +0900

    add 150A.cpp

---
+++ ABC/150A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int MOD = 1000000007;
+
+int main(){
+  int K, X;
+  cin >> K >> X;
+  if(500 * K >= X){
+    cout << "Yes" << endl;
+  }else{
+    cout << "No" << endl;
+  }
+}
