1d742598349984699d94e2d802f3a889f4caa6de
上野清雅 <seiga.ueno@gmail.com>
2020-01-24 17:01:20 +0900

    add init_prime

--- library.cpp
+++ library.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 typedef long long ll;
 
+vector<bool> prime_sieve;
+vector<ll> prime_numbers;
+void init_prime(ll n){
+  prime_sieve.resize(n+1, true);
+  prime_sieve[0] = prime_sieve[1] = false;
+  for(ll i = 2; i <= n; i++){
+    if(prime_sieve[i] == true){
+      prime_numbers.push_back(i);
+      for(ll j = 2; i * j <= n; j++){
+	prime_sieve[i * j] = false;
+      }
+    }
+  }
+}
+
 //素数か判定 O(sqrt(N))
 bool is_prime(ll N){
   if(N <= 1) return false;
@@ -24,5 +39,8 @@ ll lcm(ll a, ll b){
 
 
 int main(){
-
+  init_prime(10000);
+  for(auto x: prime_numbers){
+    cout << x << endl;
+  }
 }
