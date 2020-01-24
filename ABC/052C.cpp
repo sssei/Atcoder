f885d1066ef6c55a8ef11274b8b625f8f8e8e921
上野清雅 <seiga.ueno@gmail.com>
2020-01-24 17:02:03 +0900

    add 052C.cpp

--- ABC/052C.cpp
+++ ABC/052C.cpp
@@ -2,11 +2,32 @@
 using namespace std;
 typedef long long ll;
 
-const int MOD 1000000007;
-vector<ll> fib(10000);
+const int MOD = 1000000007;
+vector<bool> prime;
 
 int main(){
   int N;
   cin >> N;
-  for(int i = 0; i < )
+  prime.resize(N+1, true);
+  prime[0] = prime[1] = false;
+  vector<int> p;
+  for(int i = 2; i <= N; i++){
+    if(prime[i] == true){
+      p.push_back(i);
+      for(int j = 2; i * j <= N; j++){
+	prime[i * j] = false;
+      }
+    }
+  }
+  ll res = 1;
+  for(auto x: p){
+    int cnt = 0;
+    int tmp = x;
+    while(tmp <= N){
+      cnt += N / tmp;
+      tmp *= x;
+    }
+    res = res * (cnt + 1) % MOD;
+  }
+  cout << res << endl;
 }
