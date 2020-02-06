9d037a8c2f36e3f3d7b88f037aae48247551e04e
上野清雅 <seiga.ueno@gmail.com>
2020-02-06 12:07:19 +0900

    add 049C.cpp

--- ABC/049C.cpp
+++ ABC/049C.cpp
@@ -7,7 +7,43 @@ int main(){
   cin >> S;
   ll len = S.size();
   int i = 0;
+  bool flag = true;
   while(i < len){
-    if(S)
+    if(S.substr(i, 5) == "dream"){
+      i += 5;
+      if(S[i] == 'd' || S[i] == '\0' || S.substr(i, 3) == "era"){
+  	continue;
+      }else if(S.substr(i, 2) == "er"){
+  	if(S[i+2] == 'd' || S[i+2] == 'e' || S[i+2] == '\0'){
+  	  i += 2;
+  	  continue;
+  	}else{
+  	  flag = false;
+  	  break;
+  	}
+      }else{
+        flag = false;
+  	break;
+      }
+    }else if(S.substr(i, 5) == "erase"){
+      i += 5;
+      if(S[i] == '\0' || S[i] == 'd' || S[i] == 'e'){
+	continue;
+      }else if(S[i] == 'r'){
+	i++;
+	continue;
+      }else{
+	flag = false;
+	break;
+      }
+    }else{
+      flag = false;
+      break;
+    }
+  }
+  if(flag){
+    cout << "YES" << endl;
+  }else{
+    cout << "NO" << endl;
   }
 }
