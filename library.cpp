98113170b3bcfe52284d920458650162adec0600
上野清雅 <seiga.ueno@gmail.com>
2020-02-11 10:48:21 +0900

    update some

--- library.cpp
+++ library.cpp
@@ -4,6 +4,26 @@ typedef long long ll;
 const int MOD = 1000000007;
 const int MAX = 1000000;
 
+template < typename T > inline string toString( const T &a ) { ostringstream oss; oss << a; return oss.str(); };
+
+ll dp[20][2][2];
+ll digitDP(string N){
+  int L = N.size();
+  fill((ll*)dp, (ll*)dp + sizeof(dp)/sizeof(ll), 0);
+  dp[0][0][0] = 1;
+  for(int i = 0; i < L; i++){
+    int D = N[i] - '0';
+    for(int j = 0; j < 2; j++){
+      for(int k = 0; k < 2; k++){
+	for(int d = 0; d <= (j ? 9 : D); d++){
+	  dp[i+1][j || (d < D)][k || d == 4 || d == 9] += dp[i][j][k];
+	}
+      }
+    }
+  }
+  return dp[L][0][1] + dp[L][1][1];
+}
+
 class UnionFind{
 public:
   vector<ll> par; //親を表す配列
